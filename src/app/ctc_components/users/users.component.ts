bb24b11181c35b073e377877b5b45833b8caaf10
Chaitanya <chaitanya.tatavolu@gmail.com>
2020-03-30 22:41:40 -0700

    Updated User
    Updated User html,ts,scss

--- src/app/ctc_components/users/users.component.ts
+++ src/app/ctc_components/users/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { cities } from 'src/app/models/cities';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+        this.cities1 = [
+            {label:'Select User Type', value:null},
+            {label:'DataEntryOperator', value:{id:1, name: 'DataEntryOperator', code: 'NY'}},
+            {label:'Teacher', value:{id:2, name: 'Teacher', code: 'RM'}},
+            {label:'Admin', value:{id:3, name: 'Admin', code: 'LDN'}}
+        ];
+        this.userstat = [
+            {label:'Status', value:null},
+            {label:'Active', value:{id:1, name: 'Active', code: 'AC'}},
+            {label:'InActive', value:{id:2, name: 'InActive', code: 'InAC'}},
+        
+        ];
+
+   }
 
   ngOnInit(): void {
   }
