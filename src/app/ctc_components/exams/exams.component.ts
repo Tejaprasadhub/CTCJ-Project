7175d8d20576bc392f5b071f5696cf35a7da419a
sindhuja chinchilam <chinchilamsindhuja@gmail.com>
2020-03-31 17:07:29 +0530

    Update exams.component.ts
    Adding exams screen

--- src/app/ctc_components/exams/exams.component.ts
+++ src/app/ctc_components/exams/exams.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { LazyLoadEvent, SelectItem } from 'primeng/api/public_api';
+import { Exams } from 'src/app/models/exams';
+import { ExamsService } from 'src/app/services/exams.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-exams',
@@ -6,10 +12,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exams.component.scss']
 })
 export class ExamsComponent implements OnInit {
+  private ngUnsubscribe = new Subject();
+  datasource: Exams[];
+  exams: Exams[];
+  totalRecords: number;
+  cols: any[];
+  @ViewChild('myFiltersDiv') myFiltersDiv: ElementRef;
+  loading: boolean;
 
-  constructor() { }
+  constructor(private examsService: ExamsService, private router: Router) {
+    this.exams = [];
+   }
 
-  ngOnInit(): void {
+   public ngOnInit() {
+    this.examsService.getExams();
+    this.examsService.examsJson.pipe(takeUntil(this.ngUnsubscribe)).subscribe(exams => {
+      this.datasource = exams;
+      this.totalRecords = this.datasource.length;
+    });
+    this.cols = [
+      { field: 'title', header: 'Title' },
+      { field: 'year', header: 'Year' },
+      { field: 'createddate', header: 'Created Date' },
+      { field: 'createdby', header: 'Created By' }
+    ];
+    this.loading = true;
+  }
+
+  loadCarsLazy(event: LazyLoadEvent) {
+    this.loading = true;
+    setTimeout(() => {
+      if (this.datasource) {
+        this.exams = this.datasource.slice(event.first, (event.first + event.rows));
+        this.loading = false;
+      }
+    }, 1000);
+  }
+
+  toggleClass($event: any) {
+    if (this.myFiltersDiv.nativeElement.classList.contains('transform-active'))
+      this.myFiltersDiv.nativeElement.classList.remove('transform-active')
+    else
+      this.myFiltersDiv.nativeElement.classList.add('transform-active')
   }
 
 }
