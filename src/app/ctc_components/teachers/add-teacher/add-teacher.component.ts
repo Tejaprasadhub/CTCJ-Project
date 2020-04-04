eab335d160446b117da042bd06b9edbd83285fda
Teja Prasad Behara <53400473+Tejaprasadhub@users.noreply.github.com>
2020-04-05 01:35:17 +0530

    Add Teacher  Full Functionality

--- src/app/ctc_components/teachers/add-teacher/add-teacher.component.ts
+++ src/app/ctc_components/teachers/add-teacher/add-teacher.component.ts
@@ -6,6 +6,7 @@ import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { Router, ActivatedRoute } from '@angular/router';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-add-teacher',
@@ -13,12 +14,13 @@ import { Subject } from 'rxjs';
   styleUrls: ['./add-teacher.component.scss']
 })
 export class AddTeacherComponent implements OnInit {
-  gender: Gender[];
-  qualification: any;
-  experience: any;
+  gender: SelectItem[]=[];
+  qualification: SelectItem[];
+  experience: SelectItem[];
   teacherId:string;
   formType:string;
   pageTitle:string;
+  editData:any;
   isDisabled:boolean=false;
   isRequired:boolean=false;
   display:boolean=false;
@@ -31,20 +33,22 @@ export class AddTeacherComponent implements OnInit {
   formSubmitAttempt: boolean = false;
   errorMessage:string="";
   successMessage:string="";
+  
   constructor(private fb: FormBuilder, private router: Router,private route:ActivatedRoute) {
     this.gender = [
-      { name: 'Male', code: 'M' },
-      { name: 'Female', code: 'F' }
+      { label: 'Male', value: 'M' },
+      { label: 'Female', value: 'F' }
     ];
+    
     this.qualification = [
-      { name: 'B.Ed', code: 'B.Ed' },
-      { name: 'M.Ed', code: 'M.Ed' },
-      { name: 'Other', code: 'OTH' },
+      { label: 'B.Ed', value: 'B.Ed' },
+      { label: 'M.Ed', value: 'M.Ed' },
+      { label: 'Other', value: 'OTH' }
     ];
     this.experience = [
-      { name: '0-1(yrs)', code: '0-1' },
-      { name: '15-20(yrs)', code: '15-20' },
-      { name: '>20(yrs)', code: '>20' },
+      { label: '0-1(yrs)', value: '0-1' },
+      { label: '15-20(yrs)', value: '15-20' },
+      { label: '>20(yrs)', value: '>20' }
     ];
     this.expertiseIn = [
       { label: 'Telugu', value: 'T' },
@@ -85,10 +89,12 @@ export class AddTeacherComponent implements OnInit {
     }else if(this.formType == "edit"){
       this.pageTitle="Edit Teacher";
       this.editControls();
+      this.fetchData();
     }else{
       this.pageTitle="View Details";
       this.isDisabled=true;
       this.isRequired=false;
+      this.fetchData();
     }
 
   }
@@ -106,6 +112,57 @@ export class AddTeacherComponent implements OnInit {
       'associatedClasses': new FormControl('', { validators: [Validators.required] }),
       'associatedSections': new FormControl('', { validators: [Validators.required] })
     });
+  }
+  //to fetch data when edit teacher
+  private fetchData(){
+    // this.loadGender(this.gender);
+    this.bindEditTeacherDetails();
+  }
+  // loadGender(datalist:any){
+  //   this.gender=this.bindDropDownList(datalist,'gender');
+  // }
+  // bindDropDownList(datalist,type){   
+  //   let outputList:SelectItem[]=[];
+  //   outputList.push({label:"select",value:null})
+  //   if(datalist != null){
+  //     datalist.forEach(element=>{
+  //       let obj:SelectItem;
+  //       if(type=="gender"){
+  //         obj={label:element.label,value:element.value}
+  //       }
+  //       outputList.push(obj);
+  //     });
+  //   }
+  //   return outputList;
+  // }
+  //to bind data to controllers
+  bindEditTeacherDetails(){
+    this.editData={
+      'teacherName':'Teja prasad',
+      'dateofbirth': '4/5/2020',
+      'gender': 'F',
+      'qualification': 'OTH',
+      'experience': '0-1',
+      'mobile': '9640938361',
+      'email': 'tejaprasadbehara@gmail.com',
+      'expertiseIn':['T','M'],
+      'associatedClasses': ['1','2'],
+      'associatedSections': ['A','B']
+    }
+
+    console.log(this.editData.gender)
+    this.addTeacherForm.setValue({
+      'teacherName': this.editData.teacherName,
+      'dateofbirth': this.editData.dateofbirth,
+      'gender': this.editData.gender,
+      'qualification': this.editData.qualification,
+      'experience': this.editData.experience,
+      'mobile': this.editData.mobile,
+      'email': this.editData.email,
+      'expertiseIn': this.editData.expertiseIn,
+      'associatedClasses': this.editData.associatedClasses,
+      'associatedSections': this.editData.associatedSections
+    })
   }
   // Add Teacher method
   addTeacherSubmit(): void {
